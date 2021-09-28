cd03abc185265d24a887e7003d0ffa5c58736ccd
Kevin <kxkleiman@gmail.com>
2021-09-28 14:35:13 -0600

    kernel change

--- env/src/kernel.c
+++ env/src/kernel.c
@@ -1,8 +1,8 @@
 
+
 void main()
 {
-//    char* VID_MEM = (char*) 0xB8000;
-//    VID_MEM = 'Q';
-    *(char*)0xb8000 = 'Q';
+    char* VID_MEM = (char*) 0xB8000;
+    *VID_MEM = 'K';
     return;
 }
