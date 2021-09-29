543f95ab70eb659e0b6352348bce04e689a4f658
Kevin <kxkleiman@gmail.com>
2021-09-29 08:48:54 -0600

    define vga mem

--- env/src/kernel/kernel.c
+++ env/src/kernel/kernel.c
@@ -8,11 +8,11 @@ void main()
      */
     unsigned char *vid_mem = VGA_MEM;
     *vid_mem = 'K';
-    vid_mem = (unsigned char*) 0xB8000 + 2;
+    vid_mem = (unsigned char*) VGA_MEM + 2;
     *vid_mem = '-';
-    vid_mem = (unsigned char*) 0xB8000 + 4;
+    vid_mem = (unsigned char*) VGA_MEM + 4;
     *vid_mem = 'O';
-    vid_mem = (unsigned char*) 0xB8000 + 6;
+    vid_mem = (unsigned char*) VGA_MEM + 6;
     *vid_mem = 'S';
     return;
 }
