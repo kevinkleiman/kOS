daeea7c49d384fa5008f768613473c6508a6f16b
Kevin Kleiman <kxkleiman@gmail.com>
2024-03-08 13:36:26 -0500

    more cleanup

--- rbin/x86_64-kos/debug/deps/rustc_std_workspace_core-284dc67eae89f3ac.d
+++
@@ -1,7 +0,0 @@
-/Users/kevinkleiman/Projects/kOS/./rbin/x86_64-kos/debug/deps/librustc_std_workspace_core-284dc67eae89f3ac.rmeta: /Users/kevinkleiman/.rustup/toolchains/nightly-aarch64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
-
-/Users/kevinkleiman/Projects/kOS/./rbin/x86_64-kos/debug/deps/librustc_std_workspace_core-284dc67eae89f3ac.rlib: /Users/kevinkleiman/.rustup/toolchains/nightly-aarch64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
-
-/Users/kevinkleiman/Projects/kOS/./rbin/x86_64-kos/debug/deps/rustc_std_workspace_core-284dc67eae89f3ac.d: /Users/kevinkleiman/.rustup/toolchains/nightly-aarch64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
-
-/Users/kevinkleiman/.rustup/toolchains/nightly-aarch64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs:
--- rbin/x86_64-kos/debug/libkOS.d
+++
@@ -1,1 +0,0 @@
-/Users/kevinkleiman/Projects/kOS/./rbin/x86_64-kos/debug/libkOS.a: /Users/kevinkleiman/Projects/kOS/src/lib.rs
