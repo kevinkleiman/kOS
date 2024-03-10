c976146d72a8a6a8c9c79dadf73c7a51cb8ccba9
Kevin Kleiman <kxkleiman@gmail.com>
2024-03-10 17:10:03 -0400

    fixing up

--- lib/lib.rs
+++ lib/lib.rs
@@ -2,7 +2,16 @@
 #![no_std]
 #![no_main]
 
-// setup paths to src (prob not great ik)
+/**
+ *
+ * @copyright Copyright (c) 2024, Kevin Kleiman, All Rights Reserved
+ * 
+ * Yes, kOS supports Rust. This is linked as a static library to the kernel
+ * Feel free to write drivers in whichever language you choose (C, asm, Rust) 
+ *
+ * All rust functions are prefixed with __ to improve kernel readability
+ */
+
 #[path = "../src/rs/cdef.rs"] mod cdef;
 #[path = "../src/drivers/rs/driver.rs"] mod driver;
 
@@ -11,12 +20,10 @@ use core::panic::PanicInfo;
 // Check rust has been linked properly
 #[no_mangle]
 pub extern "C" fn __init() -> u32 {
-    // cdef::tty_write("\nRust static lib init.\n\0");
-
     return 1
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn _panic(_info: &PanicInfo) -> ! {
+    cdef::panic("RPANIC!\0");
 }
--- src/rs/cdef.rs
+++ src/rs/cdef.rs
@@ -2,10 +2,16 @@
 mod alias {
     extern "C" {
         pub fn tty_write(buf: *const u8);
+        pub fn panic(buf: *const u8);
     }
 }
 
-
 pub fn tty_write(buf: &str) {
     unsafe { alias::tty_write(buf.as_ptr()); }
+}
+
+
+pub fn panic(buf: &str) -> ! {
+    unsafe { alias::panic(buf.as_ptr()); }
+    loop {}
 }
